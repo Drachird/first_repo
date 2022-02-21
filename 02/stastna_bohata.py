8f083f77ac9e8cff21224e519739ce03ed593238
robertsokola <robertsokola2@gmail.com>
2022-02-21 05:39:10 +0100

    Úprava if-else pro Toma

--- 02/stastna_bohata.py
+++ 02/stastna_bohata.py
@@ -1,25 +1,22 @@
 print('Odpovídej "ano" nebo "ne".')
+
+# Lepší je deklarovat proměnné na začátku, pro přehlednost
 stastna_retezec = input('Jsi šťastná? ')
-if stastna_retezec == 'ano' or stastna_retezec == 'Ano':
-    stastna = True
-elif stastna_retezec == 'ne' or stastna_retezec == 'Ne':
-    stastna = False
-else:
-    print('Nerozumím!')
-
 bohata_retezec = input('Jsi bohatá? ')
-if bohata_retezec == 'ano' or bohata_retezec == 'Ano':
-    bohata = True
-elif bohata_retezec == 'ne' or bohata_retezec == 'Ne':
-    bohata = False
-else:
-    print('Nerozumím!')
 
-if bohata and stastna:
+# Jednodušší if-else systém, zajímá tě pouze ANO, aby ti to vrátilo True, řetězec převedeš na malá písmenka abys nemusel
+# řešit případy jako Ano, ANo, aNO, aNo atp., tyto případy ti vždy vrátí True.
+STASTNA: bool = stastna_retezec.lower() == "ano"
+BOHATA: bool = bohata_retezec.lower() == "ano"
+
+
+if BOHATA and STASTNA:
     print('Gratuluji!')
-elif bohata:
+elif BOHATA:
     print('Zkus se víc usmívat!')
-elif stastna:
+elif STASTNA:
     print('Zkus míň utrácet.')
 else:
+    # Tady ti je celkem jedno jestli ti vše ostatní vrátí False, pořád ti to vypíše To je mi líto, moc tady nepotřebuješ
+    # error handling
     print('To je mi líto.')
